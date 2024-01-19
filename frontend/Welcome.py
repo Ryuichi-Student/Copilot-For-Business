37ed97bfbaa7362945bfa45b28406bd109a1c9b5
Ryuichi-Student <113613486+Ryuichi-Student@users.noreply.github.com>
2024-01-19 20:40:47 +0000

    Quick setup

---
+++ frontend/Welcome.py
@@ -0,0 +1,15 @@
+# TODO: use this as home page.
+
+import streamlit as st
+st.markdown(" <style> div[class^='block-container'] { padding-top: 2rem; } </style> ", unsafe_allow_html=True)
+st.write("# Welcome to Copilot For Business! 👋")
+st.sidebar.success("")
+
+st.markdown(
+    """
+    ## What is this?
+    - 
+    ## What does it do?
+    - 
+"""
+)
