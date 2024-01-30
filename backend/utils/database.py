5f3017a6806f7ac7edb448f8c1c099ac8eb33fac
Haoran-Jie <2291000484@qq.com>
2024-01-30 14:01:21 +0000

    Database initial OOP structure - update - added abstract class to the Database base class to make sure it gets implemented by subclass

--- backend/utils/database.py
+++ backend/utils/database.py
@@ -1,25 +1,65 @@
+from abc import ABC, abstractmethod
+from typing import Optional, Union, List, Dict, Any
 import sqlite3
 import pandas as pd
 import pandasql as psql
 
-class Database:
-    def __init__(self, url, additionalMetadata=None):
-        self.url = url
-        self.schema = self.getSchema()
-        self.description = self.getDescriptions()
-        self.additionalMetadata = additionalMetadata if additionalMetadata else {}
+class Database(ABC):
+    def __init__(self, url: str, additionalMetadata: Optional[Dict[Any, Any]] = None):
+        self._url: str = url
+        self._schema: Dict[str, Dict[str, Dict[str, Union[str, bool, int]]]] = self.getSchema()
+        self._description: List[str] = self.getDescriptions()
+        self._tableNames: List[str] = self.getTableNames()
+        self._columnNames: List[str] = self.getColumnNames()
+        self.additionalMetadata: Dict[Any, Any] = additionalMetadata if additionalMetadata else {}
 
-    def to_str(self):
-        pass
-        
-    def query(self, code):
-        raise NotImplementedError("This method should be implemented by subclasses")
+    @property
+    def url(self) -> str:
+        return self._url
     
-    def getSchema(self):
-        return {}
+    @url.setter
+    def url(self, value: str) -> None:
+        self._url = value
+        
+    @property
+    def schema(self) -> Dict[str, Dict[str, Dict[str, Union[str, bool, int]]]]:
+        return self._schema
+    
+    @property
+    def description(self) -> List[str]:
+        return self._description
+    
+    @property
+    def tableNames(self) -> List[str]:
+        return self._tableNames
+    
+    @property
+    def columnNames(self) -> List[str]:
+        return self._columnNames
 
-    def getDescriptions(self):
-        return {}
+    @abstractmethod
+    def to_str(self) -> str:
+        pass
+
+    @abstractmethod
+    def query(self, code: str) -> pd.DataFrame:
+        pass
+
+    @abstractmethod
+    def getSchema(self) -> Dict[str, Dict[str, Dict[str, Union[str, bool, int]]]]:
+        pass
+
+    @abstractmethod
+    def getDescriptions(self) -> List[str]:
+        pass
+
+    @abstractmethod
+    def getTableNames(self) -> List[str]:
+        pass
+
+    @abstractmethod
+    def getColumnNames(self) -> List[str]:
+        pass
 
 class SQLiteDatabase(Database):
     def __init__(self, file_path, additionalMetadata=None):
@@ -28,13 +68,23 @@ class SQLiteDatabase(Database):
     def query(self, code):
         with sqlite3.connect(self.url) as conn:
             return pd.read_sql_query(code, conn)
-        
+
     def getDescriptions(self):
-        raise NotImplementedError()
-    
+        # Implement this method
+        pass
+
     def getSchema(self):
-        raise NotImplementedError()
-        
+        # Implement this method
+        pass
+    
+    def getTableNames(self):
+        # Implement this method
+        pass
+
+    def getColumnNames(self):
+        # Implement this method
+        pass
+
 
 class CSVDatabase(Database):
     def __init__(self, file_path, additionalMetadata=None):
@@ -43,9 +93,20 @@ class CSVDatabase(Database):
 
     def query(self, code):
         return psql.sqldf(code, locals())
-    
+
     def getDescriptions(self):
-        raise NotImplementedError()
-    
+        # Implement this method
+        pass
+
     def getSchema(self):
-        raise NotImplementedError()
+        # Implement this method
+        pass
+
+    def getTableNames(self):
+        # Implement this method
+        pass
+
+    def getColumnNames(self):
+        # Implement this method
+        pass
+
