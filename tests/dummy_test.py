358e55a74dc89d2425af07c5ef83091b261b561f
Abhiram Vinjamuri <54580211+ramstar3000@users.noreply.github.com>
2024-01-30 15:39:46 +0000

    Testing env

---
+++ tests/dummy_test.py
@@ -0,0 +1,16 @@
+from backend import test
+
+# Will run any functions of form test_* in this file
+# Can import directly from backend and frontend 
+    # May need to run pip install -e . in root directory
+
+# Can add @pytest.mark.skip(reason="") to skip tests
+
+def test_one():
+    print(test.test())
+
+    assert test.test() =="Backend is operational!"
+
+if __name__ == "__main__":
+    test_one()
+
