091009b5ba9215c52a41afac1b822ae7b8bfa06a
is.a.clam <isaaccylam@gmail.com>
2024-02-01 14:04:14 +0000

    wtf

--- src/backend/test.py
+++ src/backend/test.py
@@ -1,7 +1,2 @@
-# Use this if you want to test random functions
-from src.backend.utils.gpt import get_gpt_response
-import sqlite3 as sql
-import pandas as pd
-
 def dummy_test():
     return "Backend is operational!"
