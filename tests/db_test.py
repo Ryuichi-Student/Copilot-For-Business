d8140ff8bd5186508f4b7654d0b3b30f2d351c34
is.a.clam <isaaccylam@gmail.com>
2024-02-01 12:48:41 +0000

    remove db_test

--- tests/db_test.py
+++ tests/db_test.py
@@ -2,20 +2,19 @@ import sqlite3 as sql
 import pandas as pd
 
 def test_db():
-    conn = sql.connect('databases/crm1.db')
-    c = conn.cursor()
+    # conn = sql.connect('databases/crm1.db')
+    # c = conn.cursor()
 
-    c.execute("SELECT * FROM completedacct")
+    # c.execute("SELECT * FROM completedacct")
 
-    # Fetch all rows from the query result
-    rows = c.fetchall()
+    # # Fetch all rows from the query result
+    # rows = c.fetchall()
 
-    # Convert the result into a pandas DataFrame
-    df = pd.DataFrame(rows, columns=[description[0] for description in c.description])
+    # # Convert the result into a pandas DataFrame
+    # df = pd.DataFrame(rows, columns=[description[0] for description in c.description])
 
-    # Print the DataFrame
-    print(df)
+    # # Print the DataFrame
+    # print(df)
 
 if __name__ == "__main__":
-    #test_db()
-    pass
+    test_db()
