ffa382bd43de2afa6cf6bf70525232aa9a3b3581
is.a.clam <isaaccylam@gmail.com>
2024-02-01 14:04:14 +0000

    wtf

--- tests/sql_generator_test.py
+++ tests/sql_generator_test.py
@@ -2,10 +2,8 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-print(sys.path)
-
 # Import test from backend
-from src.backend.sql_generator_template import SQLGenerator
+from src.backend.sql_generator import SQLGenerator
 
 def test_parse_query():
 
