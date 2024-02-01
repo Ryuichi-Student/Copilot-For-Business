664b9bc575746d525bfd9034785ea96c4389dac0
Izzi Millar <izzi@greysmead.com>
2024-02-01 15:48:28 +0000

    very basic bar chart implementation

--- src/backend/visualisation/visualisationclass.py
+++ src/backend/visualisation/visualisationclass.py
@@ -1,13 +1,34 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
 class Visualisation:
-    def __init__(self, title) -> None:
+    def __init__(self, title, data, query):
         self.title = title
+        self.df = data
+        self.query = query
+    
+    # Generates the visualisation for the class. When the base class is used the title is displayed
+    def generate(self):
+        print(self.title)
+
+    # add a validate data function
+    # 
+
+    
+
+class PieChart(Visualisation):
+    def __init__(self, title, data, query, categories, percentages):
+        super().__init__(title, data, query)
+        self.categories = categories
+        self.percentages = percentages
 
 
-class BarChart(Visualisation):
-    def __init__(self, title, xaxis, yaxis):
-        super().__init__(title)
-        self.xaxis = xaxis
-        self.yaxis = yaxis
 
 
-bar = BarChart("title 1", "people", "cats")
+
+
+
+# df = pd.DataFrame({'lab':['A', 'B', 'C'], 'val':[10, 30, 20]})
+# df.plot(kind='line')
+# plt.show()
