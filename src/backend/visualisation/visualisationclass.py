ce3b98d703e7d0e46ea54e611a5f3d5943980f0d
Izzi Millar <izzi@greysmead.com>
2024-02-06 12:53:52 +0000

    static methods for chart description

--- src/backend/visualisation/visualisationclass.py
+++ src/backend/visualisation/visualisationclass.py
@@ -1,19 +1,43 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from abc import ABC, abstractmethod
 
 
-class Visualisation:
+class Visualisation(ABC):
     def __init__(self, title, data, query):
         self.title = title
         self.df: pd.DataFrame = data
         self.query = query
     
-    # Generates the visualisation for the class. When the base class is used the title is displayed
-    def generate(self):
-        print(self.title)
+    @staticmethod
+    @abstractmethod
+    def getChartName():
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def getChartDescription():
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def getChartParametersForActioner():
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def getChartParameterDescription():
+        pass
     
+    # Generates the visualisation for the class. When the base class is used the title is displayed
+    @abstractmethod
+    def generate(self):
+        pass
+    
+    @abstractmethod
     def validate(self):
-        return not self.df.empty
+        pass
+
 
     # add a validate data function
     # 
