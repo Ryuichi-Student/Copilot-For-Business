2ef8e8c9d2d128342c5702c9120cebd5aea62ecc
Izzi Millar <izzi@greysmead.com>
2024-02-06 12:24:22 +0000

    bar chart actioner functions

--- src/backend/visualisation/BarChart.py
+++ src/backend/visualisation/BarChart.py
@@ -12,6 +12,24 @@ class BarChart(Visualisation):
         self.xaxis = xaxis
         self.yaxis = yaxis
     
+    # functions for the actioner
+    @staticmethod
+    def getChartName():
+        return "Bar Chart"
+
+    @staticmethod
+    def getChartDescription():
+        return "This should be chosen when a bar chart is most suitable to represent the data. The following values for graph_type and graph_info should be used."
+    
+    # returns a dictionary of the parameters required from the Actioner to create a BarChart object
+    @staticmethod
+    def getChartParametersForActioner():
+        return {'title': '', 'x_axis': '', 'y_axis': ''}
+    
+    @staticmethod
+    def getChartParameterDescription():
+        return "x_axis should contain a string of the column name that should be used as the x axis of the bar chart. y_axis should contain a string of the column name that should be used as the y axis of the bar chart"
+    
     # generates a bar chart from the data frame with the x axis and y axis provided as identifiers for the data frame
     def generate(self):
         x_axis = self.df[self.xaxis]
@@ -31,6 +49,7 @@ class BarChart(Visualisation):
             return False
         else:
             return True
+    
 
 
 
