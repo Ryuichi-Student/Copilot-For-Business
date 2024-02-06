7377e11bd8dbde3c7b332ecf67064c4aa5678262
is.a.clam <isaaccylam@gmail.com>
2024-02-06 16:39:42 +0000

    don't we all sometimes hate mypy

--- src/backend/visualisation/__init__.py
+++ src/backend/visualisation/__init__.py
@@ -2,7 +2,7 @@ import importlib
 import pkgutil, os
 from src.backend.visualisation.Visualisation import Visualisation
 
-for (_, name, _) in pkgutil.iter_modules([os.path.dirname(__file__)]):
+for (_module_loader, name, _ispkg) in pkgutil.iter_modules([os.path.dirname(__file__)]):
     importlib.import_module(f'.{name}', __package__)
 
 visualisation_subclasses = [cls for cls in Visualisation.__subclasses__()]
