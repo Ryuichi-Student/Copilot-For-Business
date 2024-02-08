b0df16d2668319efb5c04a653d59d58faee34b62
Abhiram Vinjamuri <54580211+ramstar3000@users.noreply.github.com>
2024-02-08 11:54:16 +0000

    Added tests for actioner and added to UI

--- tests/test_actioner.py
+++ tests/test_actioner.py
@@ -1,8 +1,12 @@
 import sys
 import os
+import json
+
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from src.backend.actioner import Actioner
+from src.backend.utils.database import SQLiteDatabase
+
 
 
 def test_requirements():
@@ -18,6 +22,33 @@ def test_requirements():
     print(requirements)
     assert 1 < len(requirements) == len(set(requirements))
 
+def test_action_valid():
+
+    query = "Who is the most valuable customer?"
+    db = SQLiteDatabase('databases/crm_refined.sqlite3')
+    actioner = Actioner(db)
+    requirements = actioner.get_requirements(query)
+    command = actioner.get_action(requirements[0], query)
+
+    # Convert command into json
+    command = json.loads(command)
+    print(command)
+
+    assert command['status'] == 'success'
+
+def test_action_invalid():
+
+    query = "Hello"
+    db = SQLiteDatabase('databases/crm_refined.sqlite3')
+    actioner = Actioner(db)
+    requirements = actioner.get_requirements(query)
+    command = actioner.get_action(requirements[0], query)
+
+    # Convert command into json
+    command = json.loads(command)
+    print(command)
+
+    assert command['status'] == 'error'
 
 if __name__ == "__main__":
     test_requirements()
