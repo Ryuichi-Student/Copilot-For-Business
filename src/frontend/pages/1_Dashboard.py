5f373e11e6d1cce66a56ac39c6953d21bf018568
Ryuichi-Student <113613486+Ryuichi-Student@users.noreply.github.com>
2024-02-13 11:54:23 +0000

    Commit before merge

--- src/frontend/pages/1_Dashboard.py
+++ src/frontend/pages/1_Dashboard.py
@@ -1,3 +1,24 @@
 import streamlit as st
 
+sessions = st.session_state.session_storage
+
 st.header("Create/Delete Sessions")
+
+def create_session():
+    session_name = st.text_input("Enter a session name.")
+    if session_name:
+        session_id = hash(session_name)
+        sessions[session_id] = {"data": ""}
+        print(f"Session {session_id} has been created")
+        return session_id
+
+def delete_session(session_id):
+    if session_id in sessions:
+        del sessions[session_id]
+        return True
+    return False
+
+
+if st.button("Create Session"):
+    session_id = create_session()
+
