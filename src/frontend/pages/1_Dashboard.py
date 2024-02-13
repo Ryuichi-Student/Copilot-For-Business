a7700621a55df54ad72092222bf7ee8631b67b1c
Ryuichi-Student <113613486+Ryuichi-Student@users.noreply.github.com>
2024-02-13 13:44:34 +0000

    Sessions UI

--- src/frontend/pages/1_Dashboard.py
+++ src/frontend/pages/1_Dashboard.py
@@ -1,24 +1,32 @@
 import streamlit as st
+from src.backend.utils.sessions import Session_Storage
 
+# TODO: Load from persistent storage
+if "session_storage" not in st.session_state:
+    st.session_state.session_storage = Session_Storage(st.rerun)
 sessions = st.session_state.session_storage
+
+if "CREATE_SESSION" not in st.session_state:
+    st.session_state.CREATE_SESSION = False
 
 st.header("Create/Delete Sessions")
 
-def create_session():
-    session_name = st.text_input("Enter a session name.")
-    if session_name:
-        session_id = hash(session_name)
-        sessions[session_id] = {"data": ""}
-        print(f"Session {session_id} has been created")
-        return session_id
-
-def delete_session(session_id):
-    if session_id in sessions:
-        del sessions[session_id]
-        return True
-    return False
-
 
 if st.button("Create Session"):
-    session_id = create_session()
+    st.session_state.CREATE_SESSION = True
 
+if st.session_state.CREATE_SESSION:
+    session_name = st.text_input("Enter a session name.")
+    if session_name:
+        st.session_state.CREATE_SESSION = False
+        sessions.create_session(session_name)
+
+st.write("## Current Sessions")
+
+for session_id in sessions.get_sessions():
+    col1, col2 = st.columns([0.9, 0.1])
+    with col1:
+        st.write(sessions.session_data[session_id]["name"])
+    with col2:
+        if st.button("X", key=session_id):
+            sessions.delete_session(session_id)
