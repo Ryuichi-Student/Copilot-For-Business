f77c7973905bd66ad1d4d4522aa84dd029ff9757
Izzi Millar <izzi@greysmead.com>
2024-02-14 13:32:25 +0000

    commit before push

--- src/frontend/pages/2_Copilot_For_Business.py
+++ src/frontend/pages/2_Copilot_For_Business.py
@@ -1,12 +1,9 @@
 import streamlit as st
 import pandas as pd
-import json
 from src.backend.visualisation.PieChart import PieChart
 from src.backend.visualisation.LineChart import LineChart
 from src.backend.visualisation.BarChart import BarChart
-from src.backend.actioner import Actioner
-from src.backend.utils.database import SQLiteDatabase
-from src.backend.visualisation import visualisation_subclasses
+from src.frontend.pages.workflow import workflow
 
 df = pd.DataFrame({'lab':['A', 'X', 'D'], 'val':[10, 30, 20]})
 
@@ -22,17 +19,18 @@ if "chart" not in state:
 st.header("Copilot for Business")
 
 # all the user to enter a prompt
-userQuery = st.chat_input("Enter your question")
+state['userQuery'] = st.chat_input("Enter your question")
 
 
 
 # put this in a separate file
-if userQuery:
+if state['userQuery']:
     # display the user's entered prompt
-    st.text(userQuery)
-    
+    st.text(state['userQuery'])
+
+    workflow(state["userQuery"])
     # start the workflow
-    st.session_state["chart"] = True
+    state["chart"] = True
 
 
 
