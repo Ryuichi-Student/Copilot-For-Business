7a16b0d6c1d257d0fa96f16cb40dd1be1a0a0661
Mmesoma <mmesoma.okoro@outlook.com>
2024-02-27 13:17:05 +0000

    ain

--- tests/test_generalised_answer.py
+++ tests/test_generalised_answer.py
@@ -1,6 +1,6 @@
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))))
-from pandas import pd
+import pandas as pd
 
 from src.backend import generalised_answer
 
