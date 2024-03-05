43087d4b20a6b394e4cded37118df34cde831ddd
Ryuichi-Student <rt590@cam.ac.uk>
2024-03-05 10:34:44 +0000

    readded testing

---
+++ tests/backend/test_actioner.py
@@ -0,0 +1,43 @@
+import sys
+import os
+import json
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from src.backend.actioner import Actioner
+from src.backend.database import SQLiteDatabase
+
+# def test_action_valid():
+#     query = "Who is the most valuable customer?"
+#     db = SQLiteDatabase('databases/crm_refined.sqlite3')
+#     actioner = Actioner(db)
+#     requirements = actioner.get_requirements(query)
+#     command = actioner.get_action(requirements)
+#     print(command)
+#     assert command[0]['status'] == 'success'
+
+# def test_action_invalid():
+#     query = "Hello"
+#     db = SQLiteDatabase('databases/crm_refined.sqlite3')
+#     actioner = Actioner(db)
+#     requirements = actioner.get_requirements(query)
+#     command = actioner.get_action(requirements)
+#     print(command)
+#     assert command[0]['status'] == 'error'
+
+
+# def test_actioner_final_pass():
+#     query = "What are our most popular products?"
+#     db = SQLiteDatabase('databases/crm_refined.sqlite3')
+#     actioner = Actioner(db)
+#     command = actioner.get_final_action(query)
+#     print(command)
+#     assert command['status'] == 'success'
+
+# def test_actioner_final_fail():
+#     query = "Hello"
+#     db = SQLiteDatabase('databases/crm_refined.sqlite3')
+#     actioner = Actioner(db)
+#     command = actioner.get_final_action(query)
+#     print(command)
+#     assert command['status'] == 'error'
